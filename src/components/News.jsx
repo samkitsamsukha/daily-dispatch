65577c6d613b32b90055be912ea75972bf26a190
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-04-11 00:24:33 +0530

    Basic structure created, API key generated

---
+++ src/components/News.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div className='container my-3 '>
+        <h2 className='my-3'>Daily Dispatch: Top headlines</h2>
+        <div className='row'>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDescription"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDescription"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDescription"/>
+          </div>
+          
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+export default News
